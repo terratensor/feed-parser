b6ec5d353bc0e742644c5529479f79813095e0f8
agusev <audetv@gmail.com>
2024-03-04 17:22:28 +0300

    Tested search params

--- frontend/site-app/src/app/[locale]/page.tsx
+++ frontend/site-app/src/app/[locale]/page.tsx
@@ -1,17 +1,18 @@
 import { useTranslations } from 'next-intl';
-import {unstable_setRequestLocale} from 'next-intl/server';
+import { unstable_setRequestLocale } from 'next-intl/server';
 import PageLayout from '@/components/PageLayout';
 
 type Props = {
-  params: {locale: string};
+  params: { locale: string };
+  searchParams: { q: string | undefined }
 };
 
-export default function IndexPage({params: {locale}}: Props) {
+export default function IndexPage({ params: { locale }, searchParams: { q } }: Props) {
   // Enable static rendering
   unstable_setRequestLocale(locale);
-
+  console.log(q)
   const t = useTranslations('IndexPage')
-  console.log(t)
+
   return (
       <PageLayout title={t('title')}>
         <p className="max-w-[590px]">
