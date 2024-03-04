7bae346129dff9c97f84c38ef58e1d4e1c4dfcd6
agusev <audetv@gmail.com>
2024-03-04 12:10:11 +0300

    Test local switcher

--- frontend/site-app/src/i18n.ts
+++ frontend/site-app/src/i18n.ts
@@ -1,14 +1,17 @@
 import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
-
-// Can be imported from a shared config
-const locales = ['en', 'ru', 'de'];
+import {locales} from './config';
 
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: (
+        await (locale === 'ru'
+            ? // When using Turbopack, this will enable HMR for `ru`
+            import('../messages/ru.json')
+            : import(`../messages/${locale}.json`))
+    ).default
   };
 });
--- frontend/site-app/src/middleware.ts
+++ frontend/site-app/src/middleware.ts
@@ -1,16 +1,28 @@
 import createMiddleware from 'next-intl/middleware';
+import {pathnames, locales, localePrefix} from './config';
 
 export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['ru', 'en', 'de'],
-
-  // Used when no locale matches
-  defaultLocale: 'ru'
+  defaultLocale: 'ru',
+  locales,
+  pathnames,
+  localePrefix
 });
+
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(ru|de|en)/:path*']
+  matcher: [
+    // Enable a redirect to a matching locale at the root
+    '/',
+
+    // Set a cookie to remember the previous locale for
+    // all requests that have a locale prefix
+    '/(ru|de|en)/:path*',
+
+    // Enable redirects that add missing locales
+    // (e.g. `/pathnames` -> `/en/pathnames`)
+    '/((?!_next|_vercel|.*\\..*).*)'
+  ]
 };
 
 
