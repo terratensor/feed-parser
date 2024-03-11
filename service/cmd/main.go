a3b96aaff7ac1787ba7327a626fc0952c29a2145
agusev <audetv@gmail.com>
2024-03-11 14:47:18 +0300

    Added config

--- service/cmd/main.go
+++ service/cmd/main.go
@@ -2,40 +2,32 @@ package main
 
 import (
 	"github.com/mmcdole/gofeed"
+	"github.com/terratensor/feed-parser/service/internal/config"
 	"github.com/terratensor/feed-parser/service/internal/entities/feed"
 	"github.com/terratensor/feed-parser/service/internal/parser"
 	"github.com/terratensor/feed-parser/service/internal/workerpool"
 	"log"
 	"sync"
-	"time"
 )
 
 func main() {
 
+	cfg := config.MustLoad()
+
 	fp := gofeed.NewParser()
 	//fp.UserAgent = "PostmanRuntime/7.36.3"
-	ch := make(chan feed.Entry, 20)
-
-	urls := []parser.Link{
-		{Url: "http://kremlin.ru/events/all/feed/", Lang: "ru", ResourceID: 1},
-		{Url: "http://en.kremlin.ru/events/all/feed", Lang: "en", ResourceID: 1},
-		{Url: "https://mid.ru/ru/rss.php", Lang: "ru", ResourceID: 2},
-		{Url: "https://mid.ru/en/rss.php", Lang: "en", ResourceID: 2},
-		{Url: "https://mid.ru/de/rss.php", Lang: "de", ResourceID: 2},
-		{Url: "https://mid.ru/fr/rss.php", Lang: "fr", ResourceID: 2},
-		{Url: "https://mid.ru/es/rss.php", Lang: "es", ResourceID: 2},
-		{Url: "https://mid.ru/pt/rss.php", Lang: "pt", ResourceID: 2},
-		//{Url: "https://mid.ru/cn/rss.php", Lang: "ru", ResourceID: 1},
-		//{Url: "https://mid.ru/ar/rss.php", Lang: "ru", ResourceID: 1},
-		{Url: "https://function.mil.ru/rss_feeds/reference_to_general.htm?contenttype=xml", Lang: "ru", ResourceID: 3},
-		//{Url: "https://tass.ru/rss/v2.xml", Lang: "ru", ResourceID: 4},
-		//{Url: "https://tass.com/rss/v2.xml", Lang: "en", ResourceID: 4},
-	}
+	ch := make(chan feed.Entry, cfg.EntryChanBuffer)
 
 	wg := &sync.WaitGroup{}
-	for _, url := range urls {
+	for _, url := range cfg.URLS {
+
 		wg.Add(1)
-		p := parser.NewParser(url, 60*time.Second, 150*time.Second)
+		p := parser.NewParser(parser.Link{
+			Url:        url.Url,
+			Lang:       url.Lang,
+			ResourceID: url.ResourceID,
+		}, *cfg.Delay, *cfg.RandomDelay)
+
 		go p.Run(ch, fp, wg)
 	}
 
@@ -46,12 +38,6 @@ func main() {
 	go func() {
 		for {
 			task := workerpool.NewTask(func(data interface{}) error {
-				//entry := data.(*feed.Entry)
-				//log.Printf("data Url %#v\r\r\r\r", entry.Url)
-
-				//taskID := entry.ID
-				//time.Sleep(50 * time.Millisecond)
-				//fmt.Printf("Task %v processed\n", entry.Url)
 				return nil
 			}, <-ch)
 			pool.AddTask(task)
