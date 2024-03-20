4bc28224a3af4dd82019c0e45a5a0ac3305fb6a6
agusev <audetv@gmail.com>
2024-03-20 14:35:32 +0300

    Remove frontend from project

--- frontend/site-app/src/navigation.ts
+++
@@ -1,9 +0,0 @@
-import {createLocalizedPathnamesNavigation} from 'next-intl/navigation';
-import {pathnames, locales, localePrefix} from './config';
-
-export const {Link, redirect, usePathname, useRouter} =
-    createLocalizedPathnamesNavigation({
-      locales,
-      pathnames,
-      localePrefix
-    });
